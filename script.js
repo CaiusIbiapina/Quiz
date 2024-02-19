88233e3b2fc58d244a2fa367f8fa1e43ac1e4ea8
DanielIbiapina <daniel18.ibiapinabarros@gmail.com>
2024-02-19 17:10:13 -0300

    feature: dinâmica na página inicial e alguns detalhes no CSS

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-function criarQuizz() {
-    alert('deu certo');
+inicializando();
+
+function inicializando() {
+  //isLoading(true);
+  const promessa = axios.get(
+    "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+  );
+  promessa.then(carregarTodosQuizzes);
 }
 
-function quiz1() {
-    
+function carregarTodosQuizzes(resposta) {
+  const dadosDoQuizz = resposta.data;
+
+  for (let i = 0; i < 6; i++) {
+    const todosQuizzes = document.querySelector(".quizzes");
+    todosQuizzes.innerHTML += `
+    <li class="quizz">
+      <img
+      class="banner"
+      onclick="irParaQuizz()"
+      src="${dadosDoQuizz[i].image}"
+      />
+    </li>`;
+  }
+  //isLoading(false);
+}
+
+function irParaQuizz() {
+  alert("estou indo para o quizz!!");
 }
